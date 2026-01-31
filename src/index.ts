5bbc897a925451fe385cd3c60ef279afe39ac694
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-01-31 19:20:40 +0100

    lint json

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import type { Linter } from 'eslint';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginVue from 'eslint-plugin-vue';
+import pluginJsonc from 'eslint-plugin-jsonc';
 import prettier from 'eslint-config-prettier';
 
 const config: Linter.Config[] = [
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   ...pluginVue.configs['flat/recommended'],
+  ...pluginJsonc.configs['flat/recommended-with-jsonc'] as Linter.Config[],
   {
     files: ['*.vue', '**/*.vue'],
     languageOptions: {
