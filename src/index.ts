04626b6738a7622fb87912f9e79c322077237248
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-01 01:24:37 +0100

    promise rules

--- src/index.ts
+++ src/index.ts
@@ -178,7 +178,17 @@ const config: Linter.Config[] = defineConfigWithVueTs(
       "n/shebang": "error",
 
       // promise
+      "promise/always-return": "error",
       "promise/avoid-new": "error",
+      "promise/catch-or-return": "error",
+      "promise/no-callback-in-promise": "warn",
+      "promise/no-nesting": "warn",
+      "promise/no-new-statics": "error",
+      "promise/no-promise-in-callback": "warn",
+      "promise/no-return-in-finally": "warn",
+      "promise/no-return-wrap": "error",
+      "promise/param-names": "error",
+      "promise/valid-params": "warn",
       "promise/no-multiple-resolved": "error",
       "promise/no-native": "off",
       "promise/prefer-await-to-callbacks": "error",
