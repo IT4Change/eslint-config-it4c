6d21634a2657104533fa6af0092b608cb270e2a1
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-01 03:17:38 +0100

    refactor(eslint): make eslint config a ts file (#22)

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,20 +1,23 @@
 // eslint-disable-next-line n/file-extension-in-import
 import config from "./dist/index.js";
 
+import type { Linter } from "eslint";
+
 export default [
   ...config,
   { ignores: ["rules.json"] },
   {
-    files: ["scripts/**/*.ts"],
+    files: ["**/*.ts"],
     languageOptions: {
       parserOptions: {
         projectService: {
           allowDefaultProject: [
             "scripts/generate-rules-md.ts",
             "scripts/inspect-rules.ts",
+            "eslint.config.ts",
           ],
         },
       },
     },
   },
-];
+] satisfies Linter.Config[];
