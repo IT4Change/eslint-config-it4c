3362804160450c167bf7e7fd54e27c85d3916022
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-05 14:09:38 +0100

    vitest rules

---
+++ src/modules/vitest.ts
@@ -0,0 +1,18 @@
+import pluginVitest from 'eslint-plugin-vitest'
+
+import type { Linter } from 'eslint'
+
+const config: Linter.Config[] = [
+  {
+    files: ['**/*.test.ts', '**/*.test.js', '**/*.spec.ts', '**/*.spec.js'],
+    plugins: {
+      vitest: pluginVitest,
+    },
+    rules: {
+      ...(pluginVitest.configs.recommended.rules as Linter.RulesRecord),
+      ...(pluginVitest.configs.all.rules as Linter.RulesRecord),
+    },
+  },
+]
+
+export default config
