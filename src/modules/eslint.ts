4b893bf2b232f35fd71f6424e48fe7a6cc2826cf
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-05 14:41:20 +0100

    same ignore pattern for no-unused-vars as for typescript variant

--- src/modules/eslint.ts
+++ src/modules/eslint.ts
@@ -7,6 +7,10 @@ const config: Linter.Config[] = [
   {
     rules: {
       'no-console': 'error',
+      'no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
       'no-void': ['error', { allowAsStatement: true }],
     },
   },
