ba4b6ac0add855a6a089afe67cf6819a0fa36083
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-05 13:26:05 +0100

    fix(eslint): fix typescript parser (#30)

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,15 +2,4 @@ import config from './src/index'
 
 import type { Linter } from 'eslint'
 
-export default [
-  ...config,
-  { ignores: ['rules.json'] },
-  {
-    files: ['**/*.ts'],
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-      },
-    },
-  },
-] satisfies Linter.Config[]
+export default [...config, { ignores: ['rules.json'] }] satisfies Linter.Config[]
