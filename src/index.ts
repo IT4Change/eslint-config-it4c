67b46900e612a9c5560f8bb96cf1c3caf9e99ae2
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-05 10:55:43 +0100

    lint fix
    more lint fixes

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,21 @@
-import type { Linter } from "eslint";
-
 // Base modules (included in default config)
-import comments from "./modules/comments.js";
-import eslint from "./modules/eslint.js";
-import importX from "./modules/import-x.js";
-import json from "./modules/json.js";
-import node from "./modules/node.js";
-import prettier from "./modules/prettier.js";
-import promise from "./modules/promise.js";
-import security from "./modules/security.js";
-import typescript from "./modules/typescript.js";
-import yaml from "./modules/yaml.js";
+import comments from "./modules/comments";
+import eslint from "./modules/eslint";
+import graphql from "./modules/graphql";
+import importX from "./modules/import-x";
+import jest from "./modules/jest";
+import json from "./modules/json";
+import node from "./modules/node";
+import prettier from "./modules/prettier";
+import promise from "./modules/promise";
+import react from "./modules/react";
+import security from "./modules/security";
+import typescript from "./modules/typescript";
+import vue2 from "./modules/vue2";
+import vue3 from "./modules/vue3";
+import yaml from "./modules/yaml";
 
-// Optional modules (not included in default config)
-import graphql from "./modules/graphql.js";
-import jest from "./modules/jest.js";
-import react from "./modules/react.js";
-import vue2 from "./modules/vue2.js";
-import vue3 from "./modules/vue3.js";
+import type { Linter } from "eslint";
 
 // Export all modules individually
 export {
--- src/modules/json.ts
+++ src/modules/json.ts
@@ -2,7 +2,8 @@ import pluginJsonc from "eslint-plugin-jsonc";
 
 import type { Linter } from "eslint";
 
-const config: Linter.Config[] =
-  pluginJsonc.configs["flat/recommended-with-jsonc"] as Linter.Config[];
+const config: Linter.Config[] = pluginJsonc.configs[
+  "flat/recommended-with-jsonc"
+] as Linter.Config[];
 
 export default config;
