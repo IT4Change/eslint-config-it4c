4545fa19c79c173a4bdf7c18edef85c7b5325a50
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-05 11:17:41 +0100

    prettier config

---
+++ prettier.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'prettier'
+
+const config: Config = {
+  semi: false,
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  bracketSpacing: true,
+}
+
+export default config
