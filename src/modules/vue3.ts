0772e781a3a4718a7879d975e98165ed688cc5ba
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-10 14:01:12 +0100

    conflict prettier & vue3 max attributes

--- src/modules/vue3.ts
+++ src/modules/vue3.ts
@@ -3,9 +3,17 @@ import pluginVue from 'eslint-plugin-vue'
 
 import type { Linter } from 'eslint'
 
-const config: Linter.Config[] = defineConfigWithVueTs(
-  pluginVue.configs['flat/recommended'],
-  vueTsConfigs.strict,
-) as Linter.Config[]
+const config: Linter.Config[] = [
+  ...(defineConfigWithVueTs(
+    pluginVue.configs['flat/recommended'],
+    vueTsConfigs.strict,
+  ) as Linter.Config[]),
+  {
+    rules: {
+      // Prettier handles this
+      'vue/max-attributes-per-line': 'off',
+    },
+  },
+]
 
 export default config
