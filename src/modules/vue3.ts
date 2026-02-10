9c0a457f96d15562e6a64a4beedec3435a598f3d
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-10 14:23:29 +0100

    lint fix

--- src/modules/vue3.ts
+++ src/modules/vue3.ts
@@ -8,12 +8,12 @@ const vueAndTsFiles = ['**/*.vue', '**/*.ts', '**/*.tsx', '**/*.mts', '**/*.cts'
 const config: Linter.Config[] = [
   // scope configs without `files` to Vue/TS files to prevent rules like
   // @typescript-eslint/no-unused-expressions from applying to JSON etc.
-  ...(defineConfigWithVueTs(
-    pluginVue.configs['flat/recommended'],
-    vueTsConfigs.strict,
-  ) as Linter.Config[]).map((cfg) =>
-    cfg.files ? cfg : { ...cfg, files: vueAndTsFiles },
-  ),
+  ...(
+    defineConfigWithVueTs(
+      pluginVue.configs['flat/recommended'],
+      vueTsConfigs.strict,
+    ) as Linter.Config[]
+  ).map((cfg) => (cfg.files ? cfg : { ...cfg, files: vueAndTsFiles })),
   {
     files: vueAndTsFiles,
     rules: {
