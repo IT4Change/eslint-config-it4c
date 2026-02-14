c0e40156d344b1f039bee5dcd2ffc137e550d2df
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-14 14:40:01 +0100

    limit all configs of json and yaml linter to match only appropriate files

--- src/modules/yaml.ts
+++ src/modules/yaml.ts
@@ -2,6 +2,10 @@ import { configs as ymlConfigs } from 'eslint-plugin-yml'
 
 import type { Linter } from 'eslint'
 
-const config: Linter.Config[] = ymlConfigs['flat/recommended']
+const ymlFiles = ['**/*.yaml', '**/*.yml']
+
+const config: Linter.Config[] = ymlConfigs['flat/recommended'].map((entry: Linter.Config) =>
+  entry.files ? entry : { ...entry, files: ymlFiles },
+)
 
 export default config
