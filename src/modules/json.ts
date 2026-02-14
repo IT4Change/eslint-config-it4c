eccefaf894c78c9c9367fe60861d680f2e567722
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-14 14:41:42 +0100

    fix(eslint): fix json and yaml matcher to only match appropriate files on all configs (#63)

--- src/modules/json.ts
+++ src/modules/json.ts
@@ -2,8 +2,10 @@ import pluginJsonc from 'eslint-plugin-jsonc'
 
 import type { Linter } from 'eslint'
 
-const config: Linter.Config[] = pluginJsonc.configs[
-  'flat/recommended-with-jsonc'
-] as Linter.Config[]
+const jsonFiles = ['**/*.json', '**/*.json5', '**/*.jsonc']
+
+const config: Linter.Config[] = (
+  pluginJsonc.configs['flat/recommended-with-jsonc'] as Linter.Config[]
+).map((entry) => (entry.files ? entry : { ...entry, files: jsonFiles }))
 
 export default config
