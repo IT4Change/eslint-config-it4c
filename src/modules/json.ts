bfce9ade2760a5d2e4aaac529fe9b3eceea3acc4
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-22 19:08:58 +0100

    update jsonc lint + adjustments
    update jsonc rules
    
    more rules

--- src/modules/json.ts
+++ src/modules/json.ts
@@ -1,11 +1,11 @@
-import pluginJsonc from 'eslint-plugin-jsonc'
+import { configs } from 'eslint-plugin-jsonc'
 
 import type { Linter } from 'eslint'
 
 const jsonFiles = ['**/*.json', '**/*.json5', '**/*.jsonc']
 
-const config: Linter.Config[] = (
-  pluginJsonc.configs['flat/recommended-with-jsonc'] as Linter.Config[]
-).map((entry) => (entry.files ? entry : { ...entry, files: jsonFiles }))
+const config: Linter.Config[] = configs['flat/recommended-with-jsonc'].map((entry) =>
+  entry.files ? entry : { ...entry, files: jsonFiles },
+)
 
 export default config
